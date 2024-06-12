ba8e2628085b845a12d932f97664af65dfa22029
Shirjeel <rajasarjais@gmail.com>
2024-06-12 23:22:55 +0530

    Updated URLs, static file serving, CSS, and HTML templates for base and profile pages.

--- my_social_media/my_social_media/urls.py
+++ my_social_media/my_social_media/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 
@@ -25,3 +27,6 @@ urlpatterns = [
     path('user/', include('users.urls', namespace='user')),
     path('post/', include('posts.urls', namespace='post'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- my_social_media/posts/urls.py
+++ my_social_media/posts/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-app_name = 'Post'
+app_name = 'post'
 
 urlpatterns = [
     path('new-post', views.create_post, name='create_post')
